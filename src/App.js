6823732d9d9a5438c8a0a9341635a6909b321c50
Eden Pan <xadam69@gmail.com>
2019-12-27 22:21:01 +0800

    [add] react-router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import HomePage from './pages/homepage/HomePage';
+import {Route, Switch} from 'react-router-dom';
 import './App.css';
+
+const header = () => (
+  <div>this is header</div>
+);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <HomePage />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/header" component={header} />
+        </Switch>
       </div>
     );
   }
