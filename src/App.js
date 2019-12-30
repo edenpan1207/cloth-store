1f941727bbd92739714a1c7a796b9530aa493539
Eden Pan <xadam69@gmail.com>
2019-12-30 12:51:42 +0800

    [add] header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import HomePage from './pages/homepage/HomePage';
-import ShopPage from './pages/shop/shop';
+import ShopPage from './pages/shop/Shop';
+import Header from './components/header/Header';
 import {Route, Switch} from 'react-router-dom';
 import './App.css';
 
@@ -8,6 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
