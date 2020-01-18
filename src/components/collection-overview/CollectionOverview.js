3131670f8087da99d2f8160239c313b0edc9272d
y_eden <xadam69@ec-mart.com>
2020-01-18 22:30:55 +0800

    [add] collection page

--- src/components/collection-overview/CollectionOverview.js
+++ src/components/collection-overview/CollectionOverview.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {createStructuredSelector} from 'reselect';
-import {selectCollections} from '../../redux/shop/shop.selectors';
+import {selectCollectionsForPreview} from '../../redux/shop/shop.selectors';
 import CollectionPreview from '../../components/collection-preview/CollectionPreview';
 import './collection-overview.scss';
 
@@ -16,7 +16,7 @@ const CollectionOverview = ({collections}) => (
 )
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
+  collections: selectCollectionsForPreview
 })
 
 export default connect(mapStateToProps)(CollectionOverview);
--- src/pages/shop/shop.js
+++ src/pages/shop/shop.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import {Route} from 'react-router-dom'; 
 import CollectionOverview from '../../components/collection-overview/CollectionOverview';
+import Collection from '../collection/Collection';
 
-const ShopPage = () => (
+const ShopPage = ({match}) => (
   <div className="shop-page">
-    <CollectionOverview />
+    <Route exact path={`${match.path}`} component={CollectionOverview} />
+    <Route path={`${match.path}/:collectionId`} component={Collection} /> 
   </div>
 )
 
